54f2969949c847fef718bba6981f044b39754927
Yash Bharti <yashbharti@Yashs-MacBook-Pro.local>
2024-06-17 13:54:51 +0530

    39 Fonts Filtered :(

--- iscr_library/trace_image_experiment/run_letter.sh
+++ iscr_library/trace_image_experiment/run_letter.sh
@@ -8,17 +8,15 @@
 
 
 letters=(a)
-fonts=("lmodern" "spectral" "cuprum" "accanthis" "Alegreya" "algolrevived" "almendra"
-       "antiqua" "anttor" "antpolt" "gfsartemisia" "gfsartemisia-euler" "baskervald"
-       "QTBengal" "bera" "berenis" "LibreBodoni" "boisik" "tgbonum" "bookman" "QTBookmann"
-       "librecaslon" "CharisSIL" "cochineal" "concmath" "covfonts" "DejaVuSerif" "gfsdidot"
-       "droidserif" "electrum" "ETbb" "gandhi" "QTGaromand" "QTGraphLite" "ibarra"
-       "imfellEnglish" "aesupp" "mlmodern" "roboto" "venturis2" "Asana-Math" "fbb"
-       "CormorantGaramond" "bookman" "kerkis" "algolrevived" "Arvo" "bitter" "concmath"
-       "electrum" "drm" "egothic" "yfonts" "pgothic" "auncial" "carolmin" "huncial"
-       "inslrmaj" "humanist" "rotunda" "emerald" "aurical" "pbsi" "calligra" "chancery"
-       "tgchorus" "wedn" "aurical" "wesu" "weva" "oesch" "AnonymousPro" "ascii" "beramono"
-       "cascadia-code" "courier" "courierten" "GoMono" "luximono" "pandora")
+# fonts=("spectral" "cuprum" "accanthis" "Alegreya" "algolrevived" "almendra"
+#        "anttor" "antpolt" "gfsartemisia" "gfsartemisia-euler" "baskervald"
+#        "bera" "berenis" "LibreBodoni" "tgbonum" "bookman"
+#        "librecaslon" "CharisSIL" "cochineal" "concmath" "DejaVuSerif" "gfsdidot"
+#        "electrum" "ETbb" "ibarra"
+#        "imfellEnglish" "aesupp" "mlmodern" "venturis2" "fbb"
+#        "CormorantGaramond" "bookman" "kerkis" "algolrevived" "Arvo" "bitter" "concmath"
+#        "electrum" "drm" "chancery"
+#        "tgchorus" "aurical")
 # typefaces=("normal" "italic" "bold" "bolditalic" "smallcaps" "slanted")
 typefaces=("normal")
 
